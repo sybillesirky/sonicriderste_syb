38bbcb513d61f8a2ca7483035c236fdb18f1f860
Sybille Sirky <105552227+sybillesirky@users.noreply.github.com>
2023-12-16 18:34:52 +0100

    Collector functionality port

---
+++ src/tweaks/player/archetype/collectorarchetype.cpp
@@ -0,0 +1,12 @@
+#include "collectorarchetype.hpp"
+#include "handlers/player/initgeardata.hpp"
+
+// lbl_00089654 = Ring collect function.
+
+ASMUsed void Collector_RingSpeedGain(Player *player) {
+    if (player->characterArchetype != Collector) return;
+        player->speed += pSpeed(5);
+    if (~player->specialFlags & ringGear) { // SYB: Otherwise these fuckers get 3x Ring pickup.
+        player->currentAir += 2000;
+    }
+}
---
+++ src/tweaks/player/archetype/collectorarchetype.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "riders/player.hpp"
+#include "riders/gear.hpp"
+
+
+ASMUsed void Player_CollectorArchetype(Player *player);
