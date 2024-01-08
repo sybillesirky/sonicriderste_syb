22a90f2a83226c2817350375e59ae96b0d8c02e5
Sybille Sirky <105552227+sybillesirky@users.noreply.github.com>
2024-01-08 15:14:25 +0100

    Different menu colours

--- src/cosmetics/menu/menucolors.cpp
+++ src/cosmetics/menu/menucolors.cpp
@@ -1,18 +1,14 @@
 #include "menucolors.hpp"
 #include "lib/stdlib.hpp"
 
-constexpr std::array<RGBA, 11> CustomMenuColors = {{
-    {0x7f, 0xff, 0xd4},	// Aquamarineblue
-    {0x41, 0x69, 0xe1},	// Royalblue
-    {0x99, 0x00, 0x00},	// CrimsonRed
-    {0xFF, 0x00, 0xCB},	// Magenta
-    {0xFF, 0xC0, 0xCB},	// Pink
-    {0x8f, 0x00, 0xFF},	// Violet
-    {0x22, 0x8B, 0x22},	// ForestGreen
-    {0xFF, 0x70, 0x00},	// Orange
-    {0xFF, 0xD7, 0x00},	// gold
-    {0x5d, 0x43, 0x2c},	// WalnutBrown
-    {0x00, 0x00, 0x00}	// black
+constexpr std::array<RGBA, 7> CustomMenuColors = {{
+    {0x3a, 0x00, 0x63},	// Deep violet
+    {0x00, 0x3f, 0x63},	// Dark Teal
+    {0x4a, 0x4a, 0x4a},	// Grey
+    {0x84, 0xff, 0x00},	// Neon Green
+    {0x7a, 0x5e, 0x00},	// Dark Gold
+    {0xf7, 0xe7, 0xd1},	// Pumpkin Spice
+    {0xFF, 0xFF, 0xFF},	// Pure White
 }};
 
 /**
@@ -25,9 +21,12 @@ ASMUsed void RandomizeCustomMenuColors(GraphicalObject *graphicalObject, MainMen
     // originally coded by joe a tron as gecko code, ported to C++
 	constexpr auto vanillaColorCount = 6;
 	constexpr auto customColorCount = CustomMenuColors.size();
-    const u32 random = lbl_RNG_Number(customColorCount + vanillaColorCount); // +6 to account for vanilla colors
-    if (random >= vanillaColorCount) {
-        object->colorPreset = 0x3; // set it to gray to tint it better
-        graphicalObject->tint = CustomMenuColors[random - vanillaColorCount];
-    }
+    //const u32 random = lbl_RNG_Number(customColorCount + vanillaColorCount); // +6 to account for vanilla colors
+    const u32 random = lbl_RNG_Number(customColorCount);
+    // if (random >= vanillaColorCount) {
+    //     object->colorPreset = 0x3; // set it to gray to tint it better
+    //     graphicalObject->tint = CustomMenuColors[random - vanillaColorCount];
+    // }
+    object->colorPreset = 0x3;
+    graphicalObject->tint = CustomMenuColors[random];
 }
