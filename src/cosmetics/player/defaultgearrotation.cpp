a4580bdf402fdb8a46d3b6794453832a1e9c3b96
Sybille Sirky <105552227+sybillesirky@users.noreply.github.com>
2024-01-09 12:49:31 +0100

    playerType fix

--- src/cosmetics/player/defaultgearrotation.cpp
+++ src/cosmetics/player/defaultgearrotation.cpp
@@ -22,7 +22,7 @@ USED void InitFirstDefaultGear(Player *player) {
 USED void RotateNextDefaultGear(GraphicalObject *object) {
     if(object->active == 0) { return; }
 
-	const Player &player = players[object->idStruct.idIndex];
+	Player &player = players[object->idStruct.idIndex];
 	const u8 &controllerPort = player.input->port;
 
     if (player.extremeGear != ExtremeGear::Default) return;
@@ -39,6 +39,8 @@ USED void RotateNextDefaultGear(GraphicalObject *object) {
 
     // Update what ID we're on if pressing X (forward)
     if (player.input->toggleFaceButtons.hasAny(XButton) && object->idStruct.graphicalDataID == NotLockedGearSelectionID) {
+        player.playerType = 0; // Bodge to fix CSS making you a CPU fsr
+
         *playerDefaultID += 1;
         if (*playerDefaultID >= TotalCharacterAmount) {
             *playerDefaultID = 0;
@@ -50,12 +52,15 @@ USED void RotateNextDefaultGear(GraphicalObject *object) {
 
     // Update what ID we're on if pressing Y (backward)
     if (player.input->toggleFaceButtons.hasAny(YButton) && object->idStruct.graphicalDataID == NotLockedGearSelectionID) {
+        player.playerType = 0; // Bodge to fix CSS making you a CPU fsr
+
         if (*playerDefaultID == 0) {
             *playerDefaultID = TotalCharacterAmount - 1;
         }
         else {
             *playerDefaultID -= 1;
         }
+
         if (*playerDefaultID == SuperSonic || *playerDefaultID == Eggman) {
             *playerDefaultID -= 1;
         }
